da8c3313fbd600a997538fbd2b1deadaf6e5228d
Muhammad Bilal <pak993322@gmail.com>
2023-08-31 08:40:21 -0700

    pakages

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config();
 const mongoose = require("mongoose")
-const jwt = require('jsonwebtoken')
 
 
 const app = express();
@@ -15,13 +14,15 @@ app.use(cors());
 // to serve images inside public folder
 app.use(require('./routes/Counsler'))
 
-
+app.get("/" ,(req,res)=>{
+  res.send("Hello World")
+})
 const PORT = process.env.PORT;
 
 const CONNECTION =process.env.MONGODB_CONNECTION;
 mongoose
   .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  // .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 module.exports = app

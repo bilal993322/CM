6dcab0527d16ebf7518fbd7a05dcd96fcb32ed6e
Muhammad Bilal <pak993322@gmail.com>
2023-08-31 08:24:37 -0700

    pakages

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT;
 const CONNECTION =process.env.MONGODB_CONNECTION;
 mongoose
   .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  // .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
+module.exports = app
